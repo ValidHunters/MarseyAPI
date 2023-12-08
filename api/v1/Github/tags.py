b730809d8fc0bcb259046d60acdd454fc2365f82
Kaede <malchanceux@protonmail.com>
2023-12-08 02:06:38 +0200

    GetLatest gets anything

--- api/v1/Github/tags.py
+++ api/v1/Github/tags.py
@@ -1,7 +1,6 @@
 import aiohttp
 from ..config import repo
 
-
 async def GetLatestMarseyTag():
     url = f'https://api.github.com/repos/{repo}/tags'
 
@@ -9,10 +8,18 @@ async def GetLatestMarseyTag():
         async with session.get(url) as response:
             tags = await response.json()
 
-    marsey_tags = [tag for tag in tags if tag['name'].endswith('-Marsey')]
+    # Filter tags that end with '-Marsey' or '-Marsey-M'
+    marsey_tags = [tag for tag in tags if tag['name'].endswith(('-Marsey', '-Marsey-M'))]
 
-    latest_tag = marsey_tags[0]['name'].rstrip('-Marsey')[1:] if marsey_tags else "2.3.3"
-    return latest_tag
+    # Remove the suffix and the first character from the latest tag
+    latest_tag = marsey_tags[0]['name']
+    if latest_tag.endswith('-Marsey-M'):
+        latest_tag = latest_tag.rstrip('-Marsey-M')[1:]
+    elif latest_tag.endswith('-Marsey'):
+        latest_tag = latest_tag.rstrip('-Marsey')[1:]
+
+    return latest_tag if marsey_tags else "2.3.3"
+
 
 
 async def GetLatestMarseyMTag():
